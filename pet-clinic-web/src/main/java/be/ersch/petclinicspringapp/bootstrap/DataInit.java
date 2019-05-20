302cde843ce6783326ea11bb5647c4cbcb466bfc
ersch <8956871+Ersch@users.noreply.github.com>
2019-05-20 21:47:58 +0200

    Refactoring map impl. to generate ID property closes #12

--- pet-clinic-web/src/main/java/be/ersch/petclinicspringapp/bootstrap/DataInit.java
+++ pet-clinic-web/src/main/java/be/ersch/petclinicspringapp/bootstrap/DataInit.java
@@ -24,14 +24,12 @@ public class DataInit implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Toto");
         owner1.setLastName("Owner ");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Tata");
         owner2.setLastName("Owner ");
 
@@ -40,14 +38,12 @@ public class DataInit implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Riri");
         vet1.setLastName("Vet");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Loulou");
         vet2.setLastName("Vet");
 
