87ac2dc1dadf6ff87e9354f9ed1912c5656c177f
ersch <8956871+Ersch@users.noreply.github.com>
2019-05-28 22:57:08 +0200

    added PetTypeSerice

---
+++ pet-clinic-data/src/main/java/be/ersch/petclinicspringapp/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package be.ersch.petclinicspringapp.services;
+
+import be.ersch.petclinicspringapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/be/ersch/petclinicspringapp/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package be.ersch.petclinicspringapp.services.map;
+
+import be.ersch.petclinicspringapp.model.PetType;
+import be.ersch.petclinicspringapp.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType obect) {
+        super.delete(obect);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
