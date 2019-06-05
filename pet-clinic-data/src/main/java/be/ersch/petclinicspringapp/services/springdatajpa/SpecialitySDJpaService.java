3fab8389e7bd548567fc8c3274b237016ec701e5
ersch <8956871+Ersch@users.noreply.github.com>
2019-06-05 21:33:45 +0200

    added spring data jpa speciality service

---
+++ pet-clinic-data/src/main/java/be/ersch/petclinicspringapp/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,46 @@
+package be.ersch.petclinicspringapp.services.springdatajpa;
+
+import be.ersch.petclinicspringapp.model.Speciality;
+import be.ersch.petclinicspringapp.repositories.SpecialityRepository;
+import be.ersch.petclinicspringapp.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
